af091c8a7c030511cd31cb7ed25ed1cd22802c09
yueMaHello <ym5@ualberta.ca>
2019-02-01 16:03:35 -0700

    renew newsfeed

--- static/upload/newsfeed/newsfeed.js
+++ static/upload/newsfeed/newsfeed.js
@@ -1,1 +1,31 @@
-console.log('hello')
+        var newsString = '';
+        var newsCounter = 0;
+        $.getJSON( "../static/outfile.json", function( data ) {
+            newsString = data;
+
+        });
+        setInterval(myTimer, 2000);
+        setInterval(newsUpdateTime, 1800000);
+        function myTimer() {
+
+            if(newsCounter<newsString.length-1){
+                newsCounter+=1;
+            }
+            else{
+                newsCounter=0;
+            }
+            var currentNews = newsString[newsCounter];
+            if(typeof(currentNews)!=='undefined' && currentNews.indexOf('Home Page')<0 ){
+                document.getElementById("newsTitle").innerHTML = newsString[newsCounter];
+
+            }
+        }
+
+        function newsUpdateTime() {
+
+            $.getJSON( "../static/outfile.json", function( data ) {
+                newsString = data;
+
+            });
+
+        }
