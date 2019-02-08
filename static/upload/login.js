debeee0d0809f4e033136ee21d7a7ecf8cd54ea5
sluxsr <roy996812@gmail.com>
2019-02-08 16:20:23 -0700

    modified log in

--- static/upload/login.js
+++ static/upload/login.js
@@ -4,17 +4,21 @@ Creation date: Jan 25th, 2019
 Contents of file:
 	1. AJAX call to send signal from front-end to back-end
 */
-$(function() {
-		//ask for updating after 5 ms
-		setInterval(runAjax,5000);
-});
 var username;
-function runAjax() {
+document.addEventListener('keydown', function(e) {
+	if (e.keyCode == 113 || e.keyCode == 81){
+	  e.keypress = '1';
+	//   alert("pressed");
+	  PHOTO = 1;
+	  runAjax(1)
+	}
+});
+function runAjax(signal) {
 	$.ajax({
 		type: "post",
 		url: "/",
 		dataType: "json",
-		data: {'request':0},
+		data: {'request':signal},
 		success: function(result){
 			username = result;
 			window.location.href="/specialUserPage";   
