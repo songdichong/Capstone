a3f744b9be32ee8e933fad3dd90a7094e6400369
yueMaHello <ym5@ualberta.ca>
2019-02-09 21:03:30 -0700

    update stock

--- stock.py
+++ stock.py
@@ -1,5 +1,8 @@
 from pandas_datareader import data as pdr
 import fix_yahoo_finance as yf
 yf.pdr_override()
-data = pdr.get_data_yahoo("SPY", start="2017-01-01", end="2017-04-30")
+data = pdr.get_data_yahoo("SPY", start="2019-01-30", end="2019-02-09")
 print(data)
+
+
+
