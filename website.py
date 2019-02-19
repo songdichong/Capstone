2199d634c616531e762441b4eed284c9cc58e65d
sluxsr <roy996812@gmail.com>
2019-02-19 14:59:41 -0700

    face detection added.

--- website.py
+++ website.py
@@ -43,6 +43,36 @@ def execute_cmd(cmd):
 	result = p.stdout
 	return result
 
+def FaceDetection():
+	import cv2 
+	import face_recognition
+
+	video_capture = cv2.VideoCapture(0)
+	process_this_frame = True
+
+	while True:
+		# Grab a single frame of video
+		ret, frame = video_capture.read()
+		# Resize frame of video to 1/4 size for faster face recognition processing
+		small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+		# Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+		rgb_small_frame = small_frame[:, :, ::-1]
+
+		# Only process every other frame of video to save time
+		if process_this_frame:
+			detected_face = False
+			# # Find all the faces and face encodings in the current frame of video
+			# face_locations = face_recognition.face_locations(rgb_small_frame)
+			# face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+			face_landmarks_list = face_recognition.face_landmarks(rgb_small_frame)
+			for face_landmarks in face_landmarks_list:
+				detected_face = True
+				break			
+			if detected_face:
+				print("Face Detected.")
+	# Release handle to the webcam
+	video_capture.release()
 		
 @app.route('/',methods=['GET','POST'])
 def index():
@@ -86,9 +116,8 @@ def specialUserPage():
 			
 	return render_template("specialUserPage.html")
 if __name__=="__main__":
-	#~ _thread.start_new_thread(write_to_json,())
+	_thread.start_new_thread(write_to_json,())
 	# _thread.start_new_thread(remote_controller,())
+	_thread.start_new_thread(FaceDetection,())
 	app.debug=True
 	app.run(host='0.0.0.0',port=4310)
-
-
