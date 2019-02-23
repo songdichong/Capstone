f5d3cc0a16fb6f210d5b1cbfe313e0e1f4d9ad53
yueMAGekki <535940296@qq.com>
2019-02-23 18:20:26 -0500

    face raspberry

--- website.py
+++ website.py
@@ -18,6 +18,8 @@ from pyfingerprint.pyfingerprint import PyFingerprint
 import sched, time, _thread,json,io,shlex,subprocess,datetime,sqlite3
 import cv2
 import face_recognition
+import picamera
+import numpy as np
 
 ######################### Constant Division ############################
 FRONT_END_MSG_RESPOND = 3
@@ -85,42 +87,44 @@ def add_into_database(userID,username,email,preference):
 
 
 ####################### OpenCV Division ################################
-def FaceDetection(video_capture):
+def FaceDetection(camera,frame):
+	print('CAp')
 
 	process_this_frame = True
 
+
 	# Grab a single frame of video
-	ret, frame = video_capture.read()
-	# Resize frame of video to 1/4 size for faster face recognition processing
-	small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-	# Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-	rgb_small_frame = small_frame[:, :, ::-1]
+	
+	camera.capture(frame, format="rgb")
 
-	# Only process every other frame of video to save time
-	if process_this_frame:
-		detected_face = False
-		# # Find all the faces and face encodings in the current frame of video
-		# face_locations = face_recognition.face_locations(rgb_small_frame)
-		# face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-
-		face_landmarks_list = face_recognition.face_landmarks(rgb_small_frame)
-		for face_landmarks in face_landmarks_list:
-			detected_face = True
-			break
-		if detected_face:
-			print("Face Detected.")
-
-			return "True"
-	#video_capture.release()
+	face_locations = face_recognition.face_locations(frame)
+	print("Found {} faces in image.".format(len(face_locations)))
+	face_encodings = face_recognition.face_encodings(frame, face_locations)
+	if len(face_encodings)>0:
+		print('DEtected')
+		return "True"
 	return "False"
-	# Release handle to the webcam
-
+		
 def task2():
+	
 	global DETECTEDUSER
-	video_capture = cv2.VideoCapture(0)
+	#video_capture = cv2.VideoCapture(0)
+	camera = picamera.PiCamera()
+	camera.resolution = (320, 240)
+	output = np.empty((240, 320, 3), dtype=np.uint8)
 	while True:
+		
+		# enabled your camera in raspi-config and rebooted first.
 
-		DETECTEDUSER = FaceDetection(video_capture)
+		DETECTEDUSER = FaceDetection(camera,output)
+
+
+		
+		
+	#while True:
+		# Grab a single frame of video from the RPi camera as a numpy array
+
+		
 ########################################################################
 
 ########################## Flask Division ##############################
@@ -219,7 +223,7 @@ def register():
 if __name__=="__main__":
 	#_thread.start_new_thread(write_to_json,())
 	# _thread.start_new_thread(remote_controller,())
-	#~ _thread.start_new_thread(task2,())
+	_thread.start_new_thread(task2,())
 	app.debug=True
 	app.run(host='0.0.0.0',port=4310)
 ########################################################################
