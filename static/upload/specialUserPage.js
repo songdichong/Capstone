1425b9b18818c51ef3ff5c3824fd706ceeb62f29
yueMaHello <ym5@ualberta.ca>
2019-03-09 15:25:13 -0700

    preference

--- static/upload/specialUserPage.js
+++ static/upload/specialUserPage.js
@@ -11,7 +11,7 @@ var TAKE_PHOTO_SUCCESS = "photo_success";
 var LOGOUT_SUCCESS = "logout_success";
 
 
-let preference = window.location.href.split('/')[6];
+let preference = window.location.href.split('/')[6]; //calendar news stock weather
 
 document.addEventListener('keydown', function(e) {
 	if (e.keyCode == KEY_ENTER){
@@ -19,6 +19,27 @@ document.addEventListener('keydown', function(e) {
 	  runAjax(FRONT_END_MSG_TAKE_PHOTO);
 	}
 });
+
+
+if(preference[0]===0){
+	console.log('whatttt')
+	var div = document.getElementById('calendarList');
+	div.style.visibility = "hidden";
+	div.style.display = "none";
+
+}
+else if(preference[1]===0){
+
+}
+else if(preference[2]===0){
+
+}
+else if(preference[3]===0){
+
+}
+
+
+
 
 function runAjax(REQUEST) {
 	$.ajax({
