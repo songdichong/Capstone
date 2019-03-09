52ff74125b2ca7b4631ada3ffd52dc449b9ba495
songdichong <songdichong@gmail.com>
2019-03-09 16:28:38 -0700

    face

--- face_detection.py
+++ face_detection.py
@@ -1,5 +1,5 @@
 from face_recognition import face_locations,face_encodings
-# import picamera
+import picamera
 import os,time,requests
 import numpy as np
 from gpiozero import MotionSensor
@@ -12,7 +12,7 @@ def FaceDetection():
 	frame = np.empty((240, 320, 3), dtype=np.uint8)	
 	# capture a frame
 	camera.capture(frame, format="rgb")
-	for i in range(5):
+	for i in range(10):
 		# detecting faces
 		face_location = face_locations(frame)
 		face_encoding = face_encodings(frame, face_location)
@@ -25,6 +25,7 @@ def FaceDetection():
 	return False
 
 def PIRtask():
+	global DETECTEDUSER
 	DETECTEDUSER = False
 	url = "http://0.0.0.0:4310/getUserFace"
 	pir = MotionSensor(4)
@@ -34,15 +35,17 @@ def PIRtask():
 	while True:
 		if pir.motion_detected:
 			lastmove_time = time.time()
-			print(now_time) # debug purpose
-			if DETECTEDUSER == False
+			#~ print(1) # debug purpose
+			if DETECTEDUSER == False: 
+				print("false" )
 				DETECTEDUSER = True
 				isDetected = 1
 				data = {'isDetected': isDetected}
 				r = requests.post(url, data)
+				time.sleep(1)
 		else:
 			now_time = time.time()
-			if now_time - lastmove_time > 60:
+			if now_time - lastmove_time > 6:
 				if not FaceDetection():
 					# turn off monitor
 					DETECTEDUSER = False
@@ -50,7 +53,7 @@ def PIRtask():
 					data = {'isDetected': isDetected}
 					r = requests.post(url, data)
 					print("turn off screen")
-					time.sleep(0.5)
+					# time.sleep(0.5)
 				
 ########################################################################
 if __name__=="__main__":
